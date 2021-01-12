a30eca3ceb2d2fbe9104666f4cb46a375018777e
silvanarl <ramossilvana495@gmail.com>
2021-01-12 00:20:56 -0500

    prueba images

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import Layout from '@components/Layout';
 
+
+export const reportWebVitals =(metric) => {
+  console.log(metric);
+}
+
 function MyApp({ Component, pageProps }) {
     return (
       <Layout>
