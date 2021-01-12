80f466c0eed243910a311620c6d08f9ef368e6dc
silvanarl <ramossilvana495@gmail.com>
2021-01-11 21:57:59 -0500

    uso de getStaticProps y getstaticPath y obtener la data

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -1,20 +1,37 @@
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
+import Image from "next/image";
 
-const ProductItem = () => {
-    const {query: { id }} = useRouter();
-    const [product, setProduct] = useState([]);
-    useEffect(() => {
-        if(id){
-            window.fetch(`/api/avo/${id}`)
-            .then(res => res.json())
-            .then(data => setProduct(data))
+
+export const getStaticProps = async ({ params }) => {
+    const response = await fetch(`https://avo-next.vercel.app/api/avo/${params.id}`);
+    const  product = await response.json();
+    return {
+        props: {
+            product
         }
-    }, [])
+    }
+};
+export const getStaticPaths = async () => {
+    const response = await fetch('https://avo-next.vercel.app/api/avo');
+    const {data: productList} = await response.json();
+    const paths = productList.map(({ id }) => ({
+        params: {
+            id
+        }
+    }))
+    return {
+        paths,
+        fallback: false // todo lo que no este incluido en paths dara 404
+    }
+};
+
+const ProductItem = ({ product }) => {
 
     return (
         <div>
-            Producto id: {id}
+            Producto id: 
+            <h1>{product.name}</h1>
+            <Image alt={product.name} src={product.image} height="180" width="200" />
+
             
         </div>
     )
