9260ce405dabbe03138aeb948b826e6421e980a2
silvanarl <ramossilvana495@gmail.com>
2021-01-11 21:55:46 -0500

    agregue fonts

--- pages/_document.js
+++ pages/_document.js
@@ -16,6 +16,8 @@ class MyDocument extends Document {
             {/* webfont */}
             {/* stylesheet */}
             {/* script/js */}
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap" rel="stylesheet" />
         </Head>
         <body className="body">
           <Main />
